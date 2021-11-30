b8f3b3ad94897798c98fbfc0a0fefbc052b0fe06
LorenaCyn <82392845+LorenaCyn@users.noreply.github.com>
2021-11-30 12:02:55 +0100

    Update Car.py
    test

--- Car.py
+++ Car.py
@@ -20,7 +20,7 @@ class Car:
             testqq = 3000
             aaaa=5555
             test=7
-            cyati=800
+            cyati=8
             cyati=877799
             abejo=7
             TEST2=67677
